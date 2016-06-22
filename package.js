6586978267f77d0f0577aa12e40661f46397bc5d
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2016-06-22 13:15:01 +0200

    Updated dependency for dataporten

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kasperrt:accounts-dataporten',
   summary: "Login service for Dataporten accounts",
-  version: "0.1.6",
+  version: "0.1.7",
   git: 'https://github.com/kasperrt/meteor-accounts-dataporten.git',
   documentation: null
 });
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('kasperrt:dataporten@0.1.0', ['client', 'server']);
+  api.use('kasperrt:dataporten@0.1.1', ['client', 'server']);
 
   api.addFiles('dataporten_login_button.css', 'client');
 
